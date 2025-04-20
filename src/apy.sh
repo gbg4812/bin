c974072116142cb5bfab6b0570c8e2f760f442b4
Guillem Baldi <guillembaldi@gmail.com>
2025-04-20 14:40:33 +0200

    new command apy

--- src/apy.sh
+++ src/apy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apy() {
-    venvs=$(find -maxdepth 5 -type d -name .venv)
+    venvs=$(find ../../ -maxdepth 5 -type d -name .venv)
     cnt=$(echo $venvs | sed '/^\s*$/d' | wc -l)
     if [[ $cnt -eq 1 ]]; then
         source $venvs/bin/activate
