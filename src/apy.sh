4b717ed78ef070a67edbd9b51a3f5430c9fb61fa
Guillem Baldi <guillembaldi@gmail.com>
2025-05-26 22:58:59 +0200

    finxing apy

--- src/apy.sh
+++ src/apy.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 apy() {
-    venvs=$(find ../../ -maxdepth 5 -type d -name .venv)
-    cnt=$(echo $venvs | sed '/^\s*$/d' | wc -l)
+    venvs=$(find . -maxdepth 5 -type d -name .venv)
+
+    cnt=$(echo "$venvs" | sed '/^\s*$/d' | wc -l)
+
     if [[ $cnt -eq 1 ]]; then
-        source $venvs/bin/activate
+        source "$venvs"/bin/activate
     elif [[ $cnt -gt 1 ]]; then
-        selected=$(echo $venvs | fzf)
-        source $selected/bin/activate
+        selected=$(echo "$venvs" | fzf)
+        source "$selected"/bin/activate
+    elif [[ $cnt -eq 0 ]]; then
+        venvs=$(find ../../ -maxdepth 5 -type d -name .venv)
+        cnt=$(echo "$venvs" | sed '/^\s*$/d' | wc -l)
+        if [[ $cnt -eq 1 ]]; then
+            source "$venvs"/bin/activate
+        elif [[ $cnt -gt 1 ]]; then
+            selected=$(echo "$venvs" | fzf)
+            source "$selected"/bin/activate
+        fi
     fi
 
 }
